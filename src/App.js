d48526cb41682dd9058ade10868ef593fbf3d8fb
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-10-29 17:33:02 -0500

    started work on pigeon selection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
-import EggandPigeon from './Components/EggAndPigeon/EggAndPigeon';
+import React, { useState } from 'react';
+import EggAndPigeon from './Components/EggAndPigeon/EggAndPigeon';
 import Game from './Components/Game/Game';
 import Home from './Components/Home/Home';
+import './App.css';
 
 function App() {
+	//pigeon selection
+	const [pigeon, setPigeon] = useState(false);
+	const [displayGame, setDisplayGame] = useState(false);
+	const setPigeonHandler = (pigeon) => {
+		setPigeon(pigeon);
+	};
+
 	return (
 		<div className='App'>
-			<EggandPigeon />
-			<Game />
-			<Home />
+			<Home setPigeonHandler={setPigeonHandler} />
+			<EggAndPigeon />
+			<Game pigeon={pigeon} displayGame={displayGame} />
 		</div>
 	);
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import './Home.css';
+import BluePigeon from '../../Images/Pigeon/BluePigeon.png';
+import RedPigeon from '../../Images/Pigeon/Red-Pigeon.png';
+import GreenPigeon from '../../Images/Pigeon/GreenPigeon.png';
 
-const Home = () => {
-	return <div>Home Page</div>;
+const Home = (props) => {
+	return (
+		<div>
+			<header>
+				<h1>Welcome to Pigeon Trivia</h1>
+				<h2>Choose your Pigeon to play</h2>
+			</header>
+			<main className='main'>
+				<button
+					className='blue pigeon'
+					onClick={() => props.setPigeonHandler(BluePigeon)}>
+					<img src={BluePigeon} alt='blue pigeon' />
+				</button>
+				<button
+					className='red pigeon'
+					onClick={() => props.setPigeonHandler(RedPigeon)}>
+					<img src={RedPigeon} alt='red pigeon' />
+				</button>
+				<button
+					className='green pigeon'
+					onClick={() => props.setPigeonHandler(GreenPigeon)}>
+					<img src={GreenPigeon} alt='green pigeon' />
+				</button>
+			</main>
+		</div>
+	);
 };
 
 export default Home;
