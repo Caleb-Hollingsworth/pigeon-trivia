5c221784b445a09b33c6c966564fd548ce4c8b11
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-10-30 10:28:58 -0500

    fleshed out hooks for setting pigeon/egg/round/gamedisplay

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 import EggAndPigeon from './Components/EggAndPigeon/EggAndPigeon';
+import BlueEgg from './Images/Eggs/egg-blue.png';
+import RedEgg from './Images/Eggs/red-egg.png';
+import GreenEgg from './Images/Eggs/green-egg.png';
+import BluePigeon from './Images/Pigeon/BluePigeon.png';
+import RedPigeon from './Images/Pigeon/Red-Pigeon.png';
+import GreenPigeon from './Images/Pigeon/GreenPigeon.png';
 import Game from './Components/Game/Game';
 import Home from './Components/Home/Home';
 import './App.css';
@@ -7,16 +13,37 @@ import './App.css';
 function App() {
 	//pigeon selection
 	const [pigeon, setPigeon] = useState(false);
+	//display game
 	const [displayGame, setDisplayGame] = useState(false);
+	//set round
+	const [round, setRound] = useState(1);
 	const setPigeonHandler = (pigeon) => {
-		setPigeon(pigeon);
+		if (round === 1) {
+			pigeon === BluePigeon
+				? setPigeon(BlueEgg)
+				: pigeon === RedPigeon
+				? setPigeon(RedEgg)
+				: setPigeon(GreenEgg);
+		} else {
+			pigeon === BlueEgg
+				? setPigeon(BluePigeon)
+				: pigeon === RedEgg
+				? setPigeon(RedPigeon)
+				: setPigeon(GreenPigeon);
+		}
+		setDisplayGame(true);
+	};
+	const setRoundHandler = () => {
+		setRound(2);
 	};
 
 	return (
 		<div className='App'>
 			<Home setPigeonHandler={setPigeonHandler} />
-			<EggAndPigeon />
-			<Game pigeon={pigeon} displayGame={displayGame} />
+			{/* <EggAndPigeon /> */}
+			{displayGame === true && (
+				<Game pigeon={pigeon} setRoundHandler={setRoundHandler} />
+			)}
 		</div>
 	);
 }
